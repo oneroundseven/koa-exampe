f8e505bf7056df2b41ba259c8edc24a07dd8c796
roundseven <oneroundseven@gmail.com>
2017-12-20 21:04:04 +0800

    add imgs

---
+++ debug.js
@@ -0,0 +1,12 @@
+// Copyright 2017 FOCUS Inc.All Rights Reserved.
+
+/**
+ * @fileOverview koa-example
+ * @author oneroundseven@gmail.com
+ */
+const debug = require('debug');
+var say = debug('say:hello');
+var talk = debug('talk:world');
+
+say('hello');
+talk('world');
